4da291727e8a80288bdbed18b000cb7f025f8974
AishwaryaAtWork <pathakaishwarya54@gmail.com>
2025-03-30 02:52:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Sidebar from "./component/Sidebar";
+import Main from "./component/Main";
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false)
+  const [searchVal, setSearchVal] = useState('')
+  const [refresh, setRefresh] = useState(0)
+
+  useEffect(()=>{
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+  };
+  },[])
+
+  const executeSearch = (inputUrl) =>{
+
+  }
+
+  const handleKeyDown = (event) => {
+
+    if (event.ctrlKey && (event.key === "+" || event.key === "-" || event.key === "=")) {
+        event.preventDefault();
+    }
+
+    if (event.key === "Enter" && searchVal.trim() !== ""){ 
+      let storedSearches = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+      storedSearches.push(searchVal);  // Add new search value
+      localStorage.setItem("searchHistory", JSON.stringify(storedSearches));
+
+      setRefresh(prev=>prev+1)
+      executeSearch(searchVal)
+    } 
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App backdrop-blur-sm flex justify-between">
+      <div className={`h-full  transition-all duration-300 ${showSidebar ? 'w-10/12':'w-[100vw]'}`}>
+        <Main searchVal={searchVal} setSearchVal={setSearchVal} handleKeyDown={handleKeyDown}/>
+      </div>
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} setSearchVal={setSearchVal} refresh={refresh} />
     </div>
   );
 }
