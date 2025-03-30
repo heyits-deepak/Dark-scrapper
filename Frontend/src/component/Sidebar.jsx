22b032b32ba348da831a7566696f22130f5ebbe3
Aishwarya Pathak <109826222+AishwaryaAtWork@users.noreply.github.com>
2025-03-30 22:59:45 +0530

    Add files via upload

--- Frontend/src/component/Sidebar.jsx
+++ Frontend/src/component/Sidebar.jsx
@@ -18,18 +18,28 @@ const Sidebar = ({showSidebar, setShowSidebar, setSearchVal, refresh}) => {
     },[refresh])
 
     const deleteSearch = (index) => {
-        setSearchList(searchList.filter((_, i) => i !== index));
+       // Get current search history from localStorage
+      let storedSearches = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+      // Filter out the deleted item
+      storedSearches = storedSearches.filter((_, i) => i !== index);
+
+      // Update localStorage
+      localStorage.setItem("searchHistory", JSON.stringify(storedSearches));
+
+      // Update state
+      setSearchList(storedSearches);
     }
 
   return (
     <>
       <div
-        className={`h-full bg-[#0f2b34] transition-all duration-300 overflow-hidden ${
-          showSidebar ? "w-2/12 opacity-1" : "w-0 opacity-0"
+        className={`h-full bg-[#0f2b34] border-l border-teal-500 md:border-none z-50 absolute top-0 right-0 transition-all duration-300 overflow-hidden ${
+          showSidebar ? "w-[90vw] md:w-2/12 opacity-1" : "w-0 md:opacity-0"
         }`}>
-        <p className="p-3 text-sm font-semibold text-teal-500 text-nowrap overflow-hidden">Recent Searches</p>
+        <p className="p-2 md:p-3 text-sm font-semibold text-teal-500 text-nowrap overflow-hidden">Recent Searches</p>
 
-        <div className="p-3 w-full h-auto flex flex-col-reverse gap-2 text-sm text-teal-500 overflow-hidden">
+        <div className="p-2 md:p-3 w-full h-auto mt-4 md:mt-2 flex flex-col-reverse gap-2 text-sm text-teal-500 overflow-hidden">
             {searchList?.map((item, i)=>(
                 <div key={i} className="relative cursor-pointer" onMouseEnter={() => setHoveredIndex(i)}
                     onMouseLeave={() => setHoveredIndex(null)} onClick={()=>setSearchVal(item)}>
@@ -40,7 +50,10 @@ const Sidebar = ({showSidebar, setShowSidebar, setSearchVal, refresh}) => {
                     <div className={`px-2 bg-teal-900 z-20 absolute top-1 right-0 transition-all duration-200
                         ${hoveredIndex === i ? 'opacity-1': 'opacity-0'}`}>
                         <FaDeleteLeft className="text-lg text-teal-600 cursor-pointer"
-                            onClick={()=>deleteSearch(i)}/>
+                            onClick={(e)=>{
+                              e.stopPropagation();
+                              deleteSearch(i);
+                            }}/>
                     </div>
                 </div>
             ))}
@@ -50,12 +63,12 @@ const Sidebar = ({showSidebar, setShowSidebar, setSearchVal, refresh}) => {
 
       {showSidebar ? (
         <GoSidebarCollapse
-          className="absolute top-3 right-3 text-2xl text-teal-500 cursor-pointer"
+          className="z-50 absolute top-2 md:top-3 right-2 md:right-3 text-2xl text-teal-500 cursor-pointer"
           onClick={() => setShowSidebar(false)}
         />
       ) : (
         <GoSidebarExpand
-          className="absolute top-3 right-3 text-2xl text-teal-500 cursor-pointer"
+          className="z-50 absolute top-2 md:top-3 right-2 md:right-3 text-2xl text-teal-500 cursor-pointer"
           onClick={() => setShowSidebar(true)}
         />
       )}
